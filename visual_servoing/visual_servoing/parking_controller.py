f8ecfdf400edb6962e00dbdb1c65ff67b84ed215
Michael <michaelszeng@gmail.com>
2024-03-18 21:17:32 +0000

    working

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -24,19 +24,29 @@ class ParkingController(Node):
 
         self.create_subscription(ConeLocation, "/relative_cone", 
             self.relative_cone_callback, 1)
+        
+        self.TURN_RADIUS = 1  # meters; this is actually measured/calculated
 
-        self.parking_distance = 0.75 # meters; try playing with this number!
+        self.PARKING_DISTANCE = 0.75 # meters; try playing with this number!
         self.relative_x = 0
         self.relative_y = 0
 
-        self.previous_distance = None
-        self.previous_angle = None
-        self.current_distance_from_cone = None
+        self.previous_distance_error = 0
+        self.previous_angle = 0
+        self.distance_error = 0
         self.current_angle = None
 
         self.drive_mode = "forward"
 
         self.get_logger().info("Parking Controller Initialized")
+
+    def bound_variable(self, value, min_value, max_value):
+        if value < min_value:
+            return min_value
+        elif value > max_value:
+            return max_value
+        else:
+            return value
 
     def relative_cone_callback(self, msg):
         self.relative_x = msg.x_pos
@@ -49,60 +59,68 @@ class ParkingController(Node):
         angle_buffer = 0.01
         distance_buffer = 0.005
 
-        current_angle = np.arctan(self.relative_y/self.relative_x)
+        current_angle = np.arctan2(self.relative_y, self.relative_x)
         self.current_angle = current_angle
         # self.get_logger().info('Angle: ' + str(current_angle))
         current_distance_from_cone = np.sqrt((self.relative_x)**2+(self.relative_y)**2)
         self.current_distance_from_cone = current_distance_from_cone
-        current_distance_error = current_distance_from_cone - self.parking_distance
+        current_distance_error = current_distance_from_cone - self.PARKING_DISTANCE
 
 
-        x_error = (self.current_distance_from_cone - self.parking_distance) * np.cos(self.current_angle)
-        y_error = (self.current_distance_from_cone - self.parking_distance) * np.sin(self.current_angle)
+        x_error = (current_distance_from_cone - self.PARKING_DISTANCE) * np.cos(self.current_angle)
+        y_error = (current_distance_from_cone - self.PARKING_DISTANCE) * np.sin(self.current_angle)
         distance_error = np.sqrt(x_error** 2 + y_error ** 2)
+        self.distance_error = distance_error
 
+        K_p_steer = 1
+        K_i_steer = 0
+        K_d_steer = 0
 
-        flip_controls = -1 if self.relative_x < 0 else 1
-
-        K_p = 1
-        K_i = 0
-        K_d = 0
+        K_p_drive = 2
+        K_i_drive = 0
+        K_d_drive = 0
 
         # YOUR CODE HERE
         # Use relative position and your control law to set drive_cmd
         # also probably need to check self.relative_y
         
         # if the cone is anywhere behind the car, or if the car is too close to the cone, switch to "backing" mode
-        if self.relative_x < 0 or current_distance_error < 0:
+        if (self.relative_x < self.TURN_RADIUS and current_distance_error > 0) or (current_distance_error < 0 and current_angle > -1.57 and current_angle < 1.57):
             self.drive_mode = "backing"
+            flip_controls = -1
+        else:
+            self.drive_mode = "forward"
+            flip_controls = 1
+
+        self.get_logger().info('Current Mode: ' + str(self.drive_mode))
+
+        # stop condition: car is facing the cone and is at the desired distance away, plus or minus a buffer
+        if abs(current_angle) < angle_buffer and abs(current_distance_error) < distance_buffer: # buffer 
+                velocity = 0.0
 
         # car drives backwards (velocity < 0) in "backing" mode
-        if self.drive_mode == "backing":
-            # car is within(ish) the circle, too close to the cone
-            if self.relative_x < self.parking_distance:
-                velocity = min(-0.05, -abs(current_distance_from_cone))
-            # car is close to the cone, but is not facing the cone at the right angle
-            elif abs(current_angle) > (angle_buffer+0.05) and current_distance_error < (distance_buffer+0.05): 
-                velocity = min(-0.05, -abs(current_distance_from_cone))
-            # car exits backing mode
-            else:
-                self.drive_mode = "forward"
-        
-        if self.drive_mode == "forward":
-            # car's is facing the cone and is at the desired distance away, plus or minus a buffer
-            if abs(current_angle) < angle_buffer and abs(current_distance_error) < distance_buffer: # buffer 
-                velocity = 0.0
+        elif self.drive_mode == "backing":
+            #                                           car is close to the cone, but is not facing the cone at the right angle
+            # if (self.relative_x < self.TURN_RADIUS) or (abs(current_angle) > (angle_buffer+0.05) and current_distance_error < (distance_buffer+0.05)):
+            steering_angle = flip_controls * K_p_steer * (current_angle)
+            steering_angle = self.bound_variable(steering_angle, -0.34, 0.34)
+            velocity = -K_p_drive*abs(current_distance_from_cone * 0.25) - K_d_drive*abs(current_distance_error - self.previous_distance_error)
+            
+        elif self.drive_mode == "forward":
             # steering according to PID control based on the angle, velocity based on the distance error and steering angle
-            else:
-                steering_angle = flip_controls * K_p * (current_angle)
-                steering_angle = min(0.34, steering_angle) if steering_angle > 0 else max(-0.34, steering_angle)
-                velocity = flip_controls * (0.5 * abs(steering_angle) + current_distance_error * 0.25)
+            steering_angle = flip_controls * K_p_steer * (current_angle)
+            steering_angle = self.bound_variable(steering_angle, -0.34, 0.34)
+            velocity = K_p_drive*abs(current_distance_from_cone * 0.25) + K_d_drive*abs(current_distance_error - self.previous_distance_error)
 
         #################################
         drive_cmd.drive.steering_angle = min(0.34, steering_angle) if steering_angle > 0 else max(-0.34, steering_angle)
         drive_cmd.drive.speed = min(1.0, velocity) if velocity < 0 else max(-1.0, velocity)
         self.drive_pub.publish(drive_cmd)
         self.error_publisher()
+
+        self.previous_distance_error = current_distance_error
+        self.previous_angle = current_angle
+
 
     def error_publisher(self):
         """
@@ -115,8 +133,8 @@ class ParkingController(Node):
 
         # YOUR CODE HERE
         # Populate error_msg with relative_x, relative_y, sqrt(x^2+y^2)
-        error_msg.x_error = (self.current_distance_from_cone - self.parking_distance) * np.cos(self.current_angle)
-        error_msg.y_error = (self.current_distance_from_cone - self.parking_distance) * np.sin(self.current_angle)
+        error_msg.x_error = (self.current_distance_from_cone - self.PARKING_DISTANCE) * np.cos(self.current_angle)
+        error_msg.y_error = (self.current_distance_from_cone - self.PARKING_DISTANCE) * np.sin(self.current_angle)
         error_msg.distance_error = np.sqrt(error_msg.x_error** 2 + error_msg.y_error ** 2)
         #################################
         
